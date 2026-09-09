80db546667434f8048a958eaa51961ebd2e98d1c
Gilad Mendelovich <u9706927@bsmch.net>
2026-09-09 04:52:00 -0700

    feat(several): connect path to page

--- src/features/investigation-analysis/InvestigationAnalysisPage.tsx
+++ src/features/investigation-analysis/InvestigationAnalysisPage.tsx
@@ -1,5 +1,11 @@
+import { Box, Typography, } from "@mui/material";
+
 export const InvestigationAnalysisPage = () => {
     return (
-        null 
+      <Box component="main" className="development-screen">
+      <Typography component="h1" className="development-screen__title">
+        העמוד הכי טוב באתר
+      </Typography>
+    </Box> 
     )
   };
