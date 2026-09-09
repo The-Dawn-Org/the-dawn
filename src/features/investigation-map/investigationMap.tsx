d261834f49d7c9fcc565087304cd574792b7edf2
Abigail <u9783273@bsmch.net>
2026-09-09 06:21:46 -0700

    fix merge conflict

--- src/features/investigation-map/investigationMap.tsx
+++ src/features/investigation-map/investigationMap.tsx
@@ -3,8 +3,6 @@ import { Box } from "@mui/system";
 
 export const InvestigationMap = () => (
   <Box className="development-screen">
-    <Typography component="h1">
-      מפה
-    </Typography>
+    <Typography component="h1">מפה</Typography>
   </Box>
 );
