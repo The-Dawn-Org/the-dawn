a114245b2c7eaaef839b39fb9fc9afc34b3bbe21
Gili Biran <u9847526@bsmch.net>
2026-09-10 10:58:39 +0300

    feat(GenericTable.tsx, TableRow.tsx, TableColumnDefinition.ts, consts.ts): Add generic table finished v1

--- src/features/investigation-analysis/components/Table/TableRow.tsx
+++ src/features/investigation-analysis/components/Table/TableRow.tsx
@@ -1,26 +1,44 @@
 import { TableCell, TableRow } from "@mui/material";
 import type { Column } from "../../types/tableTypes";
+import { HIGHLIGHT_GREEN } from "./consts";
 
 interface GenericTableRowProps<T> {
     targetSubject: T;
     columns: Column<T>[];
+    textColor: string;
+    onRowClick?: (subject: T) => void;
   }
 
 const GenericTableRow = <T,>({
     targetSubject,
     columns,
+    textColor,
+    onRowClick,
 }: GenericTableRowProps<T>) => {
     console.log(targetSubject);
 
     return (
         <TableRow 
-        sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-            {columns.map((column) => (
+        onClick={() => onRowClick?.(targetSubject)}
+        sx={{
+            cursor: onRowClick ? "pointer" : "default",
+
+            "&:last-child td, &:last-child th": {
+              border: 0,
+            },
+            "&:hover": {
+              backgroundColor: HIGHLIGHT_GREEN,
+              // cursor: onRowClick ? "pointer" : "default",
+            },
+          }}>
+        {columns.map((column) => (
                 <TableCell 
                 key={column.key}
                 sx={{
-                    color: column.color?.(targetSubject),
-                  }}>
+                    color: column.color?.(targetSubject) ?? textColor,
+                  }}
+                align="right"
+                  >
                     {column.render(targetSubject)}
                 </TableCell>
             ))}
---
+++ src/features/investigation-analysis/components/Table/consts.ts
@@ -0,0 +1,5 @@
+export const GREEN = "#8BAE5A";
+export const YELLOW = "#D4A843";
+export const RED = "#C44536";
+export const BACKGROUND_GREEN = "#121d13";
+export const HIGHLIGHT_GREEN = "#243324";
