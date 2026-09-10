9546efc6bc5e90c47285f1071eb91b61da676f28
Aviv Biton <u9744944@bsmch.net>
2026-09-10 01:10:24 -0700

    fix(branch issue)

--- src/features/investigation-map/investigationMap.tsx
+++ src/features/investigation-map/investigationMap.tsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect, type FC } from "react";
 import { Box, Button } from "@mui/material";
 import "leaflet/dist/leaflet.css";
@@ -6,12 +5,13 @@ import { Map } from "./components/Map";
 import { OpenInFull, CloseFullscreen } from "@mui/icons-material";
 import { useEvents } from "../../hooks/useEvents";
 
-
 export const InvestigationMap: FC = () => {
   const [isFullscreen, setIsFullscreen] = useState<Boolean>(false);
   const { events, loading, error } = useEvents();
+  console.log("events: ");
+  console.log(events);
 
-  const toggleFullscreen = ():void => {
+  const toggleFullscreen = (): void => {
     setIsFullscreen((prev) => !prev);
   };
 
@@ -46,8 +46,7 @@ export const InvestigationMap: FC = () => {
             gap: 2,
             flexShrink: 0,
           }}
-        >
-        </Box>
+        ></Box>
       )}
 
       {/* Map Container */}
@@ -67,7 +66,7 @@ export const InvestigationMap: FC = () => {
           left: 0,
           right: 0,
           bottom: 0,
-          zIndex: isFullscreen ? 99999 : 1, 
+          zIndex: isFullscreen ? 99999 : 1,
         }}
       >
         {/* Map Header */}
